fcd329e6054e12223a349224e4200e2515a3bee3
Alexandre MONIER <alexandre.monier@epitech.eu>
2020-08-02 00:24:15 +0200

    Removing unused features and finish project structure and google chart implementation

---
+++ src/app.d.ts
@@ -0,0 +1,22 @@
+import Vue from "vue";
+
+declare module "./App.vue" {
+  interface Project {
+    name: string;
+    start: string | number;
+    end: string | number;
+    bttf: boolean;
+  }
+  
+  interface Module {
+    name: string;
+    projects: Array<Project>;
+  }
+  
+  interface TimelineInfo {
+    promo: string | number;
+    semester: string | number;
+    modules: Array<Module>;
+  }
+}
+
