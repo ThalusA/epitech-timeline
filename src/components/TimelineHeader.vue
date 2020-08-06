941e7e54ad2f3a4289e1b10d68a4a0c14bfaa7de
ThalusA <15985204+ThalusA@users.noreply.github.com>
2020-08-06 15:13:01 +0200

    Updating components and style

---
+++ src/components/TimelineHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <title>Epitech Timeline - Promotion {{ promotion }}</title>
+    <h1>Epitech Timeline - Promotion {{ promotion }}</h1>
+    <h2>Semester {{ semester }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop, Vue } from "vue-property-decorator";
+
+export default class App extends Vue {
+  @Prop({ type: [String, Number], default: "..." }) promotion!: string | number;
+  @Prop({ type: [String, Number], default: "..." }) semester!: string | number;
+}
+</script>
